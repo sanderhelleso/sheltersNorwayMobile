02f70d747a80ddb65b0ecdae2bc98f8d74fd3ad8
Sanderhelleso <sanderhelleso@hotmail.com>
2019-05-27 21:54:00 +0200

    Added bg for home

--- app/views/HomeView.js
+++ app/views/HomeView.js
@@ -1,23 +1,33 @@
 import React from 'react';
-import { StyleSheet, View, Text } from 'react-native';
+import { StyleSheet, View, Text, ImageBackground } from 'react-native';
+import styled from 'styled-components';
 
 const HomeView = () => {
+
+    const cover = require('../assets/cover.jpg');
+
     return (
-        <View style={styles.container}>
+        <StyledImage source={cover}>
             <Text>
                 Home
             </Text>
-        </View>
+            <StyledOverlay />
+        </StyledImage>
     )
 }
 
 export default HomeView;
 
-const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      backgroundColor: '#fff',
-      alignItems: 'center',
-      justifyContent: 'center',
-    },
-});
+const StyledOverlay = styled.View`
+    position: absolute;
+    top: 0;
+    right: 0;
+    bottom: 0;
+    left: 0;
+    background-color: #000000;
+    opacity: 0.7;
+`;
+
+const StyledImage = styled.ImageBackground`
+    flex: 1
+`;
