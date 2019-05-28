51504485dac73a23d63d78e9520a25a1600ac2c8
Sanderhelleso <sanderhelleso@hotmail.com>
2019-05-28 20:18:28 +0200

    Added default navigation options

--- App.js
+++ App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Text } from 'react-native';
 import { createStackNavigator, createAppContainer } from "react-navigation";
 
 import HomeScreen from './app/screens/HomeScreen';
@@ -8,8 +9,31 @@ import globalStyles from './app/globalStyles';
 globalStyles();
 
 const AppNavigator = createStackNavigator({
-	Home: { screen: HomeScreen },
-	ShelterMap: { screen: ShelterMapScreen }
+	Home: { 
+		screen: HomeScreen,
+		navigationOptions: {
+			header: null,
+		}
+	},
+	ShelterMap: { 
+		screen: ShelterMapScreen,
+		navigationOptions: {
+			title: 'NORGESKART'
+		}
+	},
+}, {
+	defaultNavigationOptions: {
+		headerTintColor: '#ffffff',
+		headerStyle: {
+			backgroundColor: '#f50057',
+		},
+		headerTitleStyle: {
+			color: '#ffffff',
+			letterSpacing: 1,
+			textAlign: 'right', 
+			flex: 1 
+		}, 
+	}
 });
 
 export default createAppContainer(AppNavigator);
