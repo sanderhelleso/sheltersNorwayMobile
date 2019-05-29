92b31db5ebb6889c9abdb0f5f89458b7ed12bf52
Sanderhelleso <sanderhelleso@hotmail.com>
2019-05-29 06:26:47 +0200

    Added new screens

---
+++ app/screens/ClosestShelterScreen.js
@@ -0,0 +1,8 @@
+import React from 'react-native';
+import { Text } from 'react-native';
+
+const ClosestShelterScreen = () => {
+    return <Text>Closest</Text>
+}
+
+export default ClosestShelterScreen;
---
+++ app/screens/SearchScreen.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { Text } from 'react-native';
+
+
+const SearchScreen = () => {
+    return <Text>Search</Text>
+}
+
+export default SearchScreen;
